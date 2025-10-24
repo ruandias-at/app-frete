c25f1626ece097cd033b539af2740f39d820b450
Ruan Dias <ruandiasat@gmail.com>
2025-10-24 17:57:15 -0300

    feat: asas

--- frontend/src/components/CatalogoOfertas.js
+++ frontend/src/components/CatalogoOfertas.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import axios from 'axios';
