2eacb9742aa9709565124ecb82dc57eaada58733
Ruan Dias <ruandiasat@gmail.com>
2025-10-24 18:51:03 -0300

    feat: asdhadushadhahudsshau

--- frontend/src/components/EditarOferta.js
+++ frontend/src/components/EditarOferta.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
